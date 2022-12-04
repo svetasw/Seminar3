dd0e1f73a7a6e4b002acef44cc25288d30ff1cea
Sveta <svetasw_17@mail.ru>
2022-12-04 12:41:29 +1100

    DZ19

---
+++ DZ19/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Input number: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number){
+  if (number[0]==number[4] || number[1]==number[3]){
+    Console.WriteLine($"Your number: {number} - palindrome.");
+  }
+  else Console.WriteLine($"Your number: {number} - no palindrome.");
+}
+
+if (number!.Length == 5){
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Enter the correct number");
+
